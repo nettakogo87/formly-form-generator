b061293cdf2e3366549786479df6439014a926bf
wis-vgaydukov <wisgaydukov@gmail.com>
2020-05-28 21:59:33 +0300

    add simple service for "fields" and "formly" form on preview

--- src/app/generator/containers/preview/preview.component.ts
+++ src/app/generator/containers/preview/preview.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+
+import { FormGroup } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+import { FieldsService } from '../../services/fields.service';
 
 @Component({
   selector: 'app-preview',
@@ -6,11 +14,34 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./preview.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PreviewComponent implements OnInit {
+export class PreviewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public form = new FormGroup({});
+  public model: any = {};
 
-  ngOnInit(): void {
+  public fields: FormlyFieldConfig[] = [];
+  public options: FormlyFormOptions = {};
+
+  private _destroyed$ = new Subject();
+
+  constructor(private _fieldService: FieldsService) {}
+
+  public ngOnInit(): void {
+    this._fieldService.fields$
+      .pipe(
+        takeUntil(this._destroyed$),
+      )
+      .subscribe((fields: FormlyFieldConfig[]) => {
+        this.fields = fields;
+      });
   }
 
+  public ngOnDestroy(): void {
+    this._destroyed$.next();
+    this._destroyed$.complete();
+  }
+
+  public submit(): void {
+
+  }
 }
---
+++ src/app/generator/services/fields.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+import { BehaviorSubject, Observable } from 'rxjs';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FieldsService {
+
+  private _fields$ = new BehaviorSubject<FormlyFieldConfig[]>(JSON.parse(localStorage.getItem('fields')) || []);
+
+  constructor() {
+  }
+
+  public get fields$(): Observable<FormlyFieldConfig[]> {
+    return this._fields$.asObservable();
+  }
+
+  public setFields(fields: FormlyFieldConfig[]): void {
+    this._fields$.next(fields);
+    localStorage.setItem(
+      'fields',
+      fields.toString(),
+    );
+  }
+
+  public getFields(): FormlyFieldConfig[] {
+    return this._fields$.value;
+  }
+}
