deb2e76ec381719da2470cd83bd213b8cba4032b
wis-vgaydukov <wisgaydukov@gmail.com>
2020-06-23 22:21:29 +0300

    fix project name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('form-generator app is running!');
+    expect(page.getTitleText()).toEqual('formly-form-generator app is running!');
   });
 
   afterEach(async () => {
